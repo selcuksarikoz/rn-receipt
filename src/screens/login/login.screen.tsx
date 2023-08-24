9cad7c183debca2c19ddb198a37c7a1f8b5f7b5e
selcuk sarikoz <benimpostahesabim@gmail.com>
2023-08-24 16:10:14 +0300

    proxy sayfası eklendi, navigasyon eklendi

--- src/screens/login/login.screen.tsx
+++ src/screens/login/login.screen.tsx
@@ -1,10 +1,25 @@
 import { Text } from "react-native";
 import { AppSafeAreaView } from "../../components";
+import { useEffect, useLayoutEffect } from "react";
+import type { StackScreenProps } from "@react-navigation/stack";
 
-export function AppLoginScreen() {
+interface IAppLoginScreenProps extends StackScreenProps<any> {
+
+}
+
+export function AppLoginScreen(props: IAppLoginScreenProps) {
+  
+  const { navigation } = props;
+
+  useLayoutEffect(() => {
+    setTimeout(() => {
+      navigation.replace("Main")
+    }, 2000)
+  }, []);
+
   return (
     <AppSafeAreaView>
       <Text>Login</Text>
     </AppSafeAreaView>
-  )
+  );
 }
--- src/screens/proxy/proxy.screen.tsx
+++ src/screens/proxy/proxy.screen.tsx
@@ -1,10 +1,58 @@
-import { Text } from "react-native";
-import { AppSafeAreaView } from "../../components";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import { faHome, faStar, faGear } from "@fortawesome/pro-solid-svg-icons";
+
+import { AppHomeScreen } from "../home";
+import { AppFavoritesScreen } from "../favorites";
+import { AppSettingsScreen } from "../settings";
+import { AppLoginScreen } from "../login";
+import { AppRegisterScreen } from "../register";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function Home() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Home"
+        component={AppHomeScreen}
+        options={{
+          tabBarIcon: ({ color }) => <FontAwesomeIcon icon={faHome} color={color} />,
+        }}
+      />
+      <Tab.Screen
+        name="Favorites"
+        component={AppFavoritesScreen}
+        options={{
+          tabBarIcon: ({ color }) => <FontAwesomeIcon icon={faStar} color={color} />,
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={AppSettingsScreen}
+        options={{
+          tabBarIcon: ({ color }) => <FontAwesomeIcon icon={faGear} color={color} />,
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
 
 export function AppProxyScreen() {
   return (
-    <AppSafeAreaView>
-      <Text>Proxy</Text>
-    </AppSafeAreaView>
-  )
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={AppLoginScreen} />
+        <Stack.Screen name="Register" component={AppRegisterScreen} />
+        <Stack.Screen
+          name="Main"
+          component={Home}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
